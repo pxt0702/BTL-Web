509bd1f9f4865c9a1d4527f699881fb27c4f8fea
Hansohee <22010253@st.phenikaa-uni.edu.vn>
2024-05-21 22:51:00 +0700

    img slide + thay doi kich co

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
+
 const imgs = document.querySelectorAll('.main-slider img');
 const dots = document.querySelectorAll('.dot');
 let currentImg = 0;
-const interval = 3000;
+const interval = 5000;
 let timer = setInterval(changeSlide, interval);
 
 function changeSlide() {
@@ -12,12 +13,6 @@ function changeSlide() {
 
   currentImg = (currentImg + 1) % imgs.length;
 
-  if (n != undefined) {
-    clearInterval(timer);
-    timer = setInterval(changeSlide, interval);
-    currentImg = n;
-  }
-  
   imgs[currentImg].style.opacity = 1;
   dots[currentImg].className += ' active';
 }
