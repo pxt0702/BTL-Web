7d30fc1d1910739c0706eae718040418fc97f524
pxt0702 <truonglegendary@gmail.com>
2024-06-08 23:32:47 +0700

    sua  trang phukien

--- phukien.js
+++ phukien.js
@@ -1,15 +1,25 @@
-// notification.js
+let notificationShown = false;
+
 function showNotification() {
-    document.getElementById('orderNotification').style.display = 'block';
-    window.scrollTo(0, 0);
+    if (!notificationShown) {
+        document.getElementById('orderNotification').style.display = 'block';
+        document.getElementById('orderNotification').scrollIntoView({ behavior: 'smooth', block: 'start' });
+        notificationShown = true;
+    }
 }
 
 function closeNotification() {
     document.getElementById('orderNotification').style.display = 'none';
+    notificationShown = false;
 }
 
 document.addEventListener('DOMContentLoaded', () => {
     document.querySelectorAll('.dathang').forEach(button => {
         button.addEventListener('click', showNotification);
+    });
+
+    document.getElementById('submit-btn').addEventListener('click', function(event) {
+        event.preventDefault(); 
+        showNotification();
     });
 });
